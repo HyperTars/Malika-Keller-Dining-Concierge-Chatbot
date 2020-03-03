7ec7af873d5ed41a57c71f00f8f4d20e808955ab
YixinHu0321 <yh3244@nyu.edu>
2020-03-03 18:17:06 -0500

    change export file

--- Data Processing/export_elasticsearch.py
+++ Data Processing/export_elasticsearch.py
@@ -8,8 +8,8 @@ old = pandas.read_csv('elastic_yelp.csv')
 
 new = []
 for i in range(len(old)):
-	endpoint = 'https://search-yelp-search-7ha4p5bd7kh65doy2drms2kupu.us-east-1.es.amazonaws.com/restaurants/Restaurant/' + str(i)
-	initial = "curl -XPUT https://search-yelp-search-7ha4p5bd7kh65doy2drms2kupu.us-east-1.es.amazonaws.com/restaurants/Restaurant/{} -d ".format(i)
+	endpoint = 'https://search-restaurants-2j2gtpc5xsc5w5atrwwyjun3ra.us-east-1.es.amazonaws.com/restaurants/Restaurant/' + str(i)
+	initial = "curl -XPUT https://search-restaurants-2j2gtpc5xsc5w5atrwwyjun3ra.us-east-1.es.amazonaws.com/restaurants/Restaurant/{} -d ".format(i)
 	middle =  "'"+ "{" + '"Business_ID": "{}", "Cuisine": "{}"'.format(old['Business_ID'][i],old['Cuisine'][i] ) + "}"+"' "
 	final =  "-H " + "'" + "Content-Type: application/json" + "'" 
 	full = initial + middle + final
