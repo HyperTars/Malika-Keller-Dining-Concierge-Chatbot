f913c2e4d3785b707734bfef75fd2c1e0fcbfa29
mike yan <mikeyan@10-18-204-120.dynapool.wireless.nyu.edu>
2020-03-03 10:22:45 -0500

    modify api gateway

--- apigClient.js
+++ apigClient.js
@@ -53,7 +53,7 @@ apigClientFactory.newClient = function (config) {
 
     
     // extract endpoint and path from url
-    var invokeUrl = 'https://zuhrwl06d2.execute-api.us-east-1.amazonaws.com/dinning_api_deploy';
+    var invokeUrl = 'https://vwmdfhmulh.execute-api.us-east-1.amazonaws.com/dinning_api_deploy';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
     var pathComponent = invokeUrl.substring(endpoint.length);
 
@@ -98,6 +98,24 @@ apigClientFactory.newClient = function (config) {
         
         
         return apiGatewayClient.makeRequest(chatbotPostRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.chatbotOptions = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var chatbotOptionsRequest = {
+            verb: 'options'.toUpperCase(),
+            path: pathComponent + uritemplate('/chatbot').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(chatbotOptionsRequest, authType, additionalParams, config.apiKey);
     };
     
 
