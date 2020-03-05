f49f9995df71e41c912f6f9f93d151f99e559740
HyperTars <hypertars@gmail.com>
2020-03-05 09:59:35 -0500

    update DataProcessing

--- DataProcessing/5_get_yelp_restaurant.py
+++ DataProcessing/5_get_yelp_restaurant.py
@@ -9,7 +9,7 @@ from decimal import Decimal
 
 # constants #
 # AWS config
-AWS_DB_REGION = 'us-east-1'
+AWS_DB_REGION = 'us-east-2'
 AWS_TABLE_NAME = 'Yelp_Restaurants'
 AWS_PRIMARY_KEY = 'RestaurantID'
 
@@ -30,21 +30,56 @@ YELP_REQ_HEADERS = {'Authorization': 'bearer %s' % YELP_API_KEY}
 YELP_REQ_PARAMETERS = {
     'term': 'food',
     'limit': 50,
-    'radius': 15000,
-    'offset': 200,
-    'location': 'Manhattan'}
+    'radius': 10000,
+    'location': 'Manhattan',
+    'sort_by': 'distance'}
 
 YELP_REQ_CUISINES = ['italian', 'chinese', 'mexican', 'american', 'japanese',
                      'pizza', 'healthy', 'brunch', 'korean', 'thai',
                      'vietnamese', 'indian', 'seafood', 'dessert']
 
-YELP_REQ_AREAS = ['Lower East Side, Manhattan',
+YELP_REQ_AREAS = ['Central Harlem, Manhattan',
+                  'Washington Heights, Manhattan',
+                  'East Harlem, Manhattan',
+                  'Upper West Side, Manhattan',
+                  'Upper East Side, Manhattan',
+                  'Midtown West, Manhattan',
+                  'Midtown East, Manhattan',
+                  'Chelsea, Manhattan',
+                  'Murray Hill, Manhattan',
+                  'Gramercy, Manhattan',
+                  'Greenwich, Manhattan',
+                  'East Village, Manhattan',
+                  'Soho, Manhattan',
+                  'Lower East Side, Manhattan',
+                  'Tribeca, Manhattan',
+                  'Chinatown, Manhattan',
+                  'Financial District, Manhattan']
+
+AREA_ZIP = {'Central Harlem, Manhattan': [10026, 10027, 10030, 10037, 10039],
+            'Washington Heights, Manhattan': [10031, 10032, 10033, 10034, 10040],
+            'East Harlem, Manhattan': [10029, 10035],
+            'Upper West Side, Manhattan': [10023, 10024, 10025],
+            'Upper East Side, Manhattan': [10021, 10028, 10044, 10065, 10075, 10128],
+            'Midtown West, Manhattan': [10019, 10020],
+            'Midtown East, Manhattan': [10022],
+            'Chelsea, Manhattan': [10001, 10011, 10018, 10036],
+            'Murray Hill, Manhattan': [10017],
+            'Gramercy, Manhattan': [10010, 10016],
+            'Greenwich, Manhattan': [10012, 10014],
+            'East Village, Manhattan': [10003, 10009],
+            'Soho, Manhattan': [10013],
+            'Lower East Side, Manhattan': [10002],
+            'Tribeca, Manhattan': [10282, 10007],
+            'Chinatown, Manhattan': [10038],
+            'Financial District, Manhattan': [10006, 10280, 10005, 10004]}
+
+YELP_REQ_AREAS_OLD = ['Lower East Side, Manhattan',
                   'Upper East Side, Manhattan',
                   'Upper West Side, Manhattan',
                   'Washington Heights, Manhattan',
                   'Central Harlem, Manhattan',
                   'Chelsea, Manhattan',
-                  'Manhattan',
                   'East Harlem, Manhattan',
                   'Gramercy Park, Manhattan',
                   'Greenwich, Manhattan',
@@ -67,13 +102,18 @@ def writeCSV(data):
         f_csv.writerows(data)
 
 
+# upload to DynamoDB
+def uploadToAWS(data):
+    dynamodb = boto3.resource('dynamodb', region_name=AWS_DB_REGION)
+    table = dynamodb.Table(AWS_TABLE_NAME)
+    table.put_item(Item=item)
+
+
 # init
-dynamodb = boto3.resource('dynamodb', region_name=AWS_DB_REGION)
-table = dynamodb.Table(AWS_TABLE_NAME)
 if os.path.exists(CSV_FILE):
     os.remove(CSV_FILE)
 start = time.time()
-temp = start
+area_time = start
 area_idx = 1
 total_item = 0
 
@@ -81,7 +121,7 @@ total_item = 0
 for area in YELP_REQ_AREAS:
     # itr each area
     YELP_REQ_PARAMETERS['location'] = area
-    temp = time.time()
+    area_time = time.time()
     area_restaurants = []
     area_item = 0
 
@@ -115,17 +155,19 @@ for area in YELP_REQ_AREAS:
 
             # write restaurant data to DynamoDB and local area restaurants list
             area_restaurants.append(item)
-            table.put_item(Item=item)
             area_item += 1
             total_item += 1
 
     # finsih area restaurants data
     writeCSV(area_restaurants)
-    print ('(%d/11) "%s" finished, item count: %d, time spent: %ds, total time: %ds, total item: %d'
-           % (area_idx, area, area_item, int(time.time() - temp), int(time.time() - start), total_item))
+    print ('(%d/%d) "%s" downloaded, item count: %d, total item: %d, time spent: %ds, total time: %ds'
+           % (area_idx, len(YELP_REQ_AREAS), area, area_item, total_item,
+              int(time.time() - area_time), int(time.time() - start)))
     area_idx += 1
 
-# delete redundant headers
+# delete redundant data
 csv_data = pd.read_csv(CSV_FILE)
 csv_data = csv_data[~csv_data[CSV_HEAD[0]].str.contains(CSV_HEAD[0])]
+csv_data = csv_data.drop_duplicates(subset=['RestaurantID'],
+                                    keep='first', inplace=True)
 csv_data.to_csv(CSV_FILE, index=False)
